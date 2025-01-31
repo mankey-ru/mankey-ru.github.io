08bd5e79aeaa796a5c132e143d3c6b026f142af6
Прохоренко Павел <pavel@oooinex.ru>
2025-01-31 08:08:41 +0300

    1.7.7 caps digits

--- inex/rzd-page-helper.user.js
+++ inex/rzd-page-helper.user.js
@@ -3,7 +3,8 @@
 // @namespace Violentmonkey Scripts
 // @match *://*.oooinex.ru/*
 // @match *://*.rzd.ru/*
-// @version 1.7.6
+// @version 1.7.7
+// @downloadURL https://mankey-ru.github.io/inex/rzd-page-helper.user.js
 // @grant none
 // @run-at document-end
 // ==/UserScript==
@@ -25,10 +26,14 @@
 		}
 
 		if (cont) {
-			document.querySelector('body').insertAdjacentHTML('beforeend', `<div style="background:grey;color:#fff;padding:3px 0;position:fixed;bottom:0;right:0;z-index:1;">${cont}</div>`)
+			document
+				.querySelector('body')
+				.insertAdjacentHTML(
+					'beforeend',
+					`<div style="background:grey;color:#fff;padding:3px 0;position:fixed;bottom:0;right:0;z-index:1;">${cont}</div>`
+				);
 		}
-	}
-	else {
+	} else {
 		pageId = rootSys?.querySelector?.('page_id').innerHTML;
 	}
 
@@ -38,18 +43,18 @@
 		errBlock.style.display = 'block';
 	}
 
-	const {
-		hostname
-	} = window.location;
+	const { hostname } = window.location;
 	let ptkCode;
 	if (hostname.includes('oooinex')) {
 		ptkCode = hostname.split('.')[1];
-	}
-	else if (hostname.startsWith('test-')) {
-		ptkCode = 'po'
-	}
-	else {
-		ptkCode = 'pr'
+		if (ptkCode.startsWith('rtest')) {
+			const rtestNum = ptkCode.split('rtest')[1];
+			ptkCode = 'r' + rtestNum;
+		}
+	} else if (hostname.startsWith('test-')) {
+		ptkCode = 'po';
+	} else {
+		ptkCode = 'pr';
 	}
 
 	if (ptkCode !== 'oooinex' && ptkCode !== 'wflow') {
@@ -61,7 +66,8 @@
 		// таймер - для страниц, которые меняют тайтл в процессе загрузки, например, в админке
 		const titleChangeTimer = setInterval(() => {
 			const prefix = `${ptkCode} `;
-			if (!document.title.startsWith(prefix) && document.title !== prefix.trim()) { // trim - для кейса когда тайтл состоит только из префикса, дело в том, что тайтл автоматом делает trim
+			if (!document.title.startsWith(prefix) && document.title !== prefix.trim()) {
+				// trim - для кейса когда тайтл состоит только из префикса, дело в том, что тайтл автоматом делает trim
 				const continuation = isXML ? pageId : document.title;
 				document.title = prefix + continuation;
 			}
@@ -69,71 +75,81 @@
 			if (titleChangeCnt >= titleChangeCntMax) {
 				clearInterval(titleChangeTimer);
 			}
-		}, titleChangeInterval)
+		}, titleChangeInterval);
 	}
 
-	function toSmallCaps (str) {
-		if (!str) throw new Error("String cannot be empty");
-		if (typeof str !== "string") throw new TypeError("String must be a string");
+	function toSmallCaps(str) {
+		if (!str) throw new Error('String cannot be empty');
+		if (typeof str !== 'string') throw new TypeError('String must be a string');
 		const smallCapsMap = {
-			A: "ᴀ",
-			B: "ʙ",
-			C: "ᴄ",
-			D: "ᴅ",
-			E: "ᴇ",
-			F: "ғ",
-			G: "ɢ",
-			H: "ʜ",
-			I: "ɪ",
-			J: "ᴊ",
-			K: "ᴋ",
-			L: "ʟ",
-			M: "ᴍ",
-			N: "ɴ",
-			O: "ᴏ",
-			P: "ᴘ",
-			Q: "ǫ",
-			R: "ʀ",
-			S: "s",
-			T: "ᴛ",
-			U: "ᴜ",
-			V: "ᴠ",
-			W: "ᴡ",
-			X: "x",
-			Y: "ʏ",
-			Z: "ᴢ",
-			a: "ᴀ",
-			b: "ʙ",
-			c: "ᴄ",
-			d: "ᴅ",
-			e: "ᴇ",
-			f: "ғ",
-			g: "ɢ",
-			h: "ʜ",
-			i: "ɪ",
-			j: "ᴊ",
-			k: "ᴋ",
-			l: "ʟ",
-			m: "ᴍ",
-			n: "ɴ",
-			o: "ᴏ",
-			p: "ᴘ",
-			q: "ǫ",
-			r: "ʀ",
-			s: "s",
-			t: "ᴛ",
-			u: "ᴜ",
-			v: "ᴠ",
-			w: "ᴡ",
-			x: "x",
-			y: "ʏ",
-			z: "ᴢ",
-			" ": " ",
+			A: 'ᴀ',
+			B: 'ʙ',
+			C: 'ᴄ',
+			D: 'ᴅ',
+			E: 'ᴇ',
+			F: 'ғ',
+			G: 'ɢ',
+			H: 'ʜ',
+			I: 'ɪ',
+			J: 'ᴊ',
+			K: 'ᴋ',
+			L: 'ʟ',
+			M: 'ᴍ',
+			N: 'ɴ',
+			O: 'ᴏ',
+			P: 'ᴘ',
+			Q: 'ǫ',
+			R: 'ʀ',
+			S: 's',
+			T: 'ᴛ',
+			U: 'ᴜ',
+			V: 'ᴠ',
+			W: 'ᴡ',
+			X: 'x',
+			Y: 'ʏ',
+			Z: 'ᴢ',
+			a: 'ᴀ',
+			b: 'ʙ',
+			c: 'ᴄ',
+			d: 'ᴅ',
+			e: 'ᴇ',
+			f: 'ғ',
+			g: 'ɢ',
+			h: 'ʜ',
+			i: 'ɪ',
+			j: 'ᴊ',
+			k: 'ᴋ',
+			l: 'ʟ',
+			m: 'ᴍ',
+			n: 'ɴ',
+			o: 'ᴏ',
+			p: 'ᴘ',
+			q: 'ǫ',
+			r: 'ʀ',
+			s: 's',
+			t: 'ᴛ',
+			u: 'ᴜ',
+			v: 'ᴠ',
+			w: 'ᴡ',
+			x: 'x',
+			y: 'ʏ',
+			z: 'ᴢ',
+			' ': ' ',
+			0: '𝟢',
+			1: '𝟣',
+			2: '𝟤',
+			3: '𝟥',
+			4: '𝟦',
+			5: '𝟧',
+			6: '𝟨',
+			7: '𝟩',
+			8: '𝟪',
+			9: '𝟫',
 		};
 
 		return str
-			.split("")
+			.split('')
 			.map((char) => smallCapsMap[char] || char)
-			.join("");
+			.join('');
 	}
-})()
+})();
