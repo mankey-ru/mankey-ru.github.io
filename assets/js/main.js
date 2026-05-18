780532c64954381266b45e09477d0d6fb1827d65
pavel <pavel@oooinex.ru>
2026-05-18 15:48:55 +0700

    README update

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,8 +2,7 @@
 	initTopBtn();
 	initLazyImages();
 
-	function initTopBtn () {
-
+	function initTopBtn() {
 		const mybutton = document.getElementById('btn-back-to-top');
 
 		let scrollTimeout;
@@ -14,7 +13,9 @@
 
 			scrollTimeout = window.requestAnimationFrame(() => {
 				const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-				scrollTop > 20 ? mybutton.classList.add('visible') : mybutton.classList.remove('visible');
+				scrollTop > 20
+					? mybutton.classList.add('visible')
+					: mybutton.classList.remove('visible');
 			});
 		});
 
@@ -23,11 +24,11 @@
 		});
 	}
 
-	function initLazyImages () {
-		document.querySelectorAll('details').forEach(details => {
+	function initLazyImages() {
+		document.querySelectorAll('details').forEach((details) => {
 			details.addEventListener('toggle', () => {
 				if (details.open) {
-					details.querySelectorAll('img[data-src]').forEach(img => {
+					details.querySelectorAll('img[data-src]').forEach((img) => {
 						img.src = img.dataset.src;
 						img.removeAttribute('data-src');
 					});
